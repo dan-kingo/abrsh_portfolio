152e12c3551af644d7d246a3b4fe093b4fd2fa2f
Dan Kingo <danieldejen23@gmail.com>
2024-05-23 11:19:14 +0300

    finish about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 };
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,61 @@
+import "./About.css";
+import profilePic from "../../assets/about_profile.svg";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <h1>About Me</h1>
+      <div className="abouts">
+        <div className="about-img">
+          <img src={profilePic} alt="" />
+        </div>
+        <div className="about-para">
+          <p className="p1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+            neque harum. Enim totam dolore eum. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Adipisci, neque harum.
+          </p>
+          <p className="p2">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
+            explicabo quae nobis iure provident. Saepe eum, impedit sit vero
+            ducimus repellat. Commodi! explicabo quae nobis iure provident.
+            Saepe eum, impedit sit vero ducimus repellat.
+          </p>
+
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>Capcut</p> <hr style={{ width: "90%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Motion Graphics</p> <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Visual Effects </p>
+              <hr style={{ width: "65%" }} />
+            </div>
+            <div className="about-skill">
+              <p> Storytelling </p>
+              <hr style={{ width: "95%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="achievements">
+        <div className="each-achieve">
+          <h3>2+</h3>
+          <p>Years of Experience</p>
+        </div>
+        <div className="each-achieve">
+          <h3>15+</h3>
+          <p>Project Completed</p>
+        </div>
+        <div className="each-achieve">
+          <h3>3+</h3>
+          <p>Happy Clients</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
