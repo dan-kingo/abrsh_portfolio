53a6577869e3b090abdb3e7f6e02f42583fe1ce0
Dan Kingo <danieldejen23@gmail.com>
2024-05-24 09:12:07 +0300

    add functionality to the email field

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import emailPic from "../../assets/mail_icon.svg";
 import phonePic from "../../assets/call_icon.svg";
 import locationPic from "../../assets/location_icon.svg";
+
 const Contact = () => {
+  const [formStatus, setFormStatus] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    formData.append("access_key", "1aab92dc-a0b9-4043-a390-b72203d93691");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      setFormStatus("Successfully sent!");
+      setEmail("");
+      setMessage("");
+      setTimeout(() => {
+        setFormStatus("");
+      }, 5000);
+    } else {
+      setFormStatus("Failed to send. Please try again.");
+    }
+  };
+
   return (
     <div className="contact-section" id="contact">
       <div className="contact-heading">
@@ -33,13 +68,16 @@ const Contact = () => {
           </div>
         </div>
         <div className="right-div">
-          <form>
+          <form onSubmit={onSubmit} method="POST">
             <label htmlFor="email">Your Email</label>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
             <label htmlFor="message">Your Message</label>
             <textarea
@@ -47,8 +85,12 @@ const Contact = () => {
               rows="5"
               id="message"
               placeholder="enter your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
             ></textarea>
             <input type="submit" name="submit" id="submitBtn" />
+            {formStatus && <p className="form-status">{formStatus}</p>}
           </form>
         </div>
       </div>
